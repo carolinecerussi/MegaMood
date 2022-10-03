cbce9a36b81243b780b161191d5cd6071262cbcf
Caroline Cerussi <caroceru@gmail.com>
2022-10-03 00:49:58 -0700

    add breadcrumbs.jsx file

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "react-router-dom";
+const Breadcrumb = () => {
+	return (
+		<nav aria-label="breadcrumb">
+			<ol className ="breadcrumb rounded-0">
+				<li className="breadcrumb-item">
+					<Link to="/" title= "Megamood">
+						MegaMood 
+					</Link>
+					</li>
+					<li className="breadcrumb-item active" aria-current="page">
+					T-Shirts</li>
+					</ol> 
+		</nav>
+	);
+};
+export default Breadcrumb;
