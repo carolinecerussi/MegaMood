0047a1fdce0bf21a0dc0e0947e06ddc4e2a193d9
Caroline Cerussi <caroceru@gmail.com>
2022-10-04 19:54:31 -0700

    rewrite tshirt control with tshirt clicked functionality

--- src/components/TShirt.js
+++ src/components/TShirt.js
@@ -1,36 +1,50 @@
-//shirts? //
+
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
-import TShirtDetail from "./TShirtDetail";
+import styled from 'styled-components';
 
-// const ClickedTShirt = (
-//   <ul>
-// 		<li><h1>{props.name}</h1></li>
-//     <li>Artist: {props.artist}</li>
-//     <li>Price: {props.price} </li>
-//     <li>Cherries</li>
-//   </ul>
-// );
+
+// const howMany = //figure out how to write howMany equals quantity selection ...maybe do a number select and change to string ?
+
+const TShirtObject = styled.div`
+h1{
+	 margin: .5 rem;
+  background-color: transparent;
+  font-family: karla ;
+  color: black;
+  font-weight: 300;
+}
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+	  &:active {
+    cursor: pointer;
+    opacity: 0.4;
+  }
+`;
+
 
 function TShirt(props) {
 	return (
-		<TShirt onClick={() => props.whenTShirtClicked(props.id)}>
-		<TShirtDetail />
-		</TShirt>
-
-	)
+		<TShirtObject onClick={ () => props.whenTShirtClicked(props.id)}>
+			<h1><Image>{props.img}</Image></h1>
+			<p> Artist :{props.artist}</p>
+			<p>Price: {props.price}</p>
+			<p></p>
+		</TShirtObject>
+	);
 }
 
 TShirt.propTypes = {
 name: PropTypes.string,
 id: PropTypes.string,
 size: PropTypes.string,
-price: PropTypes.number,
+price: PropTypes.string,
 color: PropTypes.string,
-quantity: PropTypes.number,
-artist: PropTypes.string
-//create whenTShirtClicked: PropTypes.func
-}
+quantity: PropTypes.string,
+artist: PropTypes.string,
+whenTShirtClicked: PropTypes.func
+};
 
 export default TShirt;
---
+++ src/components/TShirtControl.js
@@ -0,0 +1,36 @@
+//figure out a when tshirt image is clicked function to show tshirt detail
+
+import React from "react";
+import TShirt from "./TShirt";
+
+function TShirtControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+}
+
+
+ whenTShirtClicked() = {
+ selectedTshirt : props.id;
+ return (
+  <React.Fragment>
+  <TShirtDetail />
+  </React.Fragment/>
+ )
+}
+
+tShirts.push({
+  name: doc.data().name,
+  artist: doc.data().artist,
+  price: doc.data().price,
+  quantity: doc.data().quantity,
+  size: doc.data().size,
+  id: doc.id
+});
+});
+setMainTShirtList(tShirts);
+},
+error => {
+  SpeechSynthesisErrorEvent(error.message);
+}
+);
+return() = unSubscribe();
+},[];
